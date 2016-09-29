8f1e9d979d5a1b85432ab1396b9c91d223a22790
Marty Schoch <marty.schoch@gmail.com>
2016-09-29 15:58:56 -0400

    update for bleve API changes

--- mapping.go
+++ mapping.go
@@ -3,9 +3,10 @@ package main
 import (
 	"github.com/blevesearch/bleve"
 	"github.com/blevesearch/bleve/analysis/language/en"
+	"github.com/blevesearch/bleve/mapping"
 )
 
-func buildMapping() *bleve.IndexMapping {
+func buildMapping() mapping.IndexMapping {
 	enFieldMapping := bleve.NewTextFieldMapping()
 	enFieldMapping.Analyzer = en.AnalyzerName
 
